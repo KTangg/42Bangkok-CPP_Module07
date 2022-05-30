8692e2d9bdca8b8442615f9ecce4dc4692eec2fb
KTangg <k.tangtangtang@gmail.com>
2022-05-30 10:34:16 +0700

    third arguments using unsigned int

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: spoolpra <spoolpra@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 09:46:33 by spoolpra          #+#    #+#             */
-/*   Updated: 2022/05/30 09:58:05 by spoolpra         ###   ########.fr       */
+/*   Updated: 2022/05/30 10:33:47 by spoolpra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 
 # include <iostream>
 
-template <typename T> void	iter(T *array, unsigned int lenght, void (*f)(T &element))
+template <typename T> void	iter(T *array, unsigned int lenght, void (*f)(unsigned int, T &))
 {
 	for (unsigned int i = 0; i < lenght; i++)
 	{
-		f(array[i]);
+		f(i, array[i]);
 	}
 }
 
-template <typename T> void	print(T &element)
+template <typename T> void	print(unsigned int i, T &element)
 {
-	std::cout << element << std::endl;
+	std::cout << "Index " << i << ": " << element << std::endl;
 }
 
 #endif
